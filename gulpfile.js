05cea9ab7d93c3ca0396059d81658af2c6e47ffb
annaVyacheslavovna <anya.kuznetsova19@gmail.com>
2016-08-19 23:00:27 +0600

    page about complete

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
 var gulp = require("gulp");
+var connect = require ("gulp-connect");
+var    livereload = require('gulp-livereload');
 
-gulp.task("mytask", function(){
-	console.log("Привет");
+// Запуск сервера 
+gulp.task("connect", function(){
+	connect.server({
+		root: "app",
+		livereload:true,
+		port: 8888
+	});
 });
+
+// Работа с html 
+gulp.task("html", function () {
+          gulp.src("./app/*.html")
+          .pipe(connect.reload());
+});
+
+// Работа с css 
+gulp.task("css", function(){
+		gulp.src("./app/css/*.css")
+		.pipe(connect.reload());	
+});
+
+// Слежка
+gulp.task ("watch", function(){
+	gulp.watch(["./app/*.html"], ["html"]);
+	gulp.watch(["./app/css/*.css"], ["css"]);
+});
+
+// Задача по умолачанию
+gulp.task("default", ["connect","watch","html","css"]);
